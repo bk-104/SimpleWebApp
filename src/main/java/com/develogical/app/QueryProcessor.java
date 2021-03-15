c7bbf88edf11a6d2aa3553088b0cd5aec24537e7
bettina <b@k.de>
2021-03-15 15:10:48 +0100

    Commit#1: added query response for oxford

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -2,6 +2,16 @@ package com.develogical.app;
 
 public class QueryProcessor {
     public String process(String query) {
+        if (query.toLowerCase().contains("oxford")) {
+            return "Oxford is the county town and only city of Oxfordshire, England." +
+                    " In 2017, its population was recorded at 152,450. It is 56 miles (90 km) northwest of London," +
+                    " 64 miles (103 km) southeast of Birmingham, and 61 miles (98 km) northeast of Bristol.\n" +
+                    "\n" +
+                    "The city is home to the University of Oxford, the oldest university in the English-speaking world," +
+                    " and has buildings in every style of English architecture from late Anglo-Saxon. " +
+                    "Oxford's industries include motor manufacturing, education, publishing, " +
+                    "information technology and science.";
+        }
         if (query.toLowerCase().contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
