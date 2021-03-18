97ef93580956c68488ce1ee73c88c6fb914dbc5a
mba <mba2019@gmx.de>
2021-03-18 16:29:53 +0100

    master: Updated QueryProcessor

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -17,7 +17,7 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
-        if (query.toLowerCase().contains("mickeymouse")) {
+        if (query.toLowerCase().contains("mickey mouse")) {
             return "Mickey Mouse (unknown) was a mouse " +
                     "Best friend of pluto the dog " +
                     "Created by walt disney.";
